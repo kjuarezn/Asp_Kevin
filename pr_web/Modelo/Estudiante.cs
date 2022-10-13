1a925fbcb3d54e7a7f007bfaf911d4c6b1eb000a
kjuarezn <70358983+kjuarezn@users.noreply.github.com>
2022-10-12 22:45:07 -0600

    .
    .

---
+++ pr_web/Modelo/Estudiante.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Web.UI.WebControls;
+using System.Globalization;
+
+namespace Modelo
+{
+
+    public class Estudiante
+    {
+        ConexionBD conectar;
+        private DataTable drop_sangre()
+        {
+            DataTable tabla = new DataTable();
+            conectar = new ConexionBD();
+            conectar.AbrirConexion();
+            string consulta = string.Format("select id_tipo_sangre as id,sangre from tipos_sangre;");
+            MySqlDataAdapter query = new MySqlDataAdapter(consulta, conectar.conectar);
+            query.Fill(tabla);
+            conectar.CerrarConexion();
+            return tabla;
+        }
+        private DataTable grid_estudiantes()
+        {
+            DataTable tabla = new DataTable();
+            conectar = new ConexionBD();
+            conectar.AbrirConexion();
+            string consulta = string.Format("select e.id_estudiante as id,e.codigo,e.nombres,e.apellidos,e.direccion,e.telefono,e.correo_electronico,e.fecha_nacimiento,t.sangre,e.id_tipo_sangre\r\nFrom estudiantes as e inner join tipos_sangres as t on e.id_tipo_sangre = t.id_tipo_sangre;");
+            MySqlDataAdapter query = new MySqlDataAdapter(consulta, conectar.conectar);
+            query.Fill(tabla);
+            conectar.CerrarConexion();
+            return tabla;
+        }
+public void drop_tipo_sangre(DropDownList drop){
+            drop.ClearSelection();
+            drop.Items.Clear();
+            drop.AppendDataBoundItems = true;
+            drop.Items.Add("<<Elige Tipo Sangre>>");
+            drop.Items[0].Value = "0";
+            drop.DataSource = drop_tipo_sangre();
+            drop.DataTextField = "sangre";
+            drop.DataValueField = "id";
+            drop.DataBind();
+
+        }
+        public void grid_estudiantes(GridView grid)
+        {
+            grid.DataSource = grid_estudiantes();
+            grid.DataBind();
+        }
+        public int crear(string codigo, string nombres, string apellidos, string direccion, string telefono, string correo, string fecha, int id_tipo_sangre)
+        {
+            int no = 0;
+            conectar = new ConexionBD();
+            conectar.AbrirConexion();
+            string consulta = string.Format("INSERT INTO estudiantes (codigo,nombres,apellidos,direccion,telefono,correo_electronico,fecha_nacimiento,id_tipo_sangre) VALUES('{0}','{1}','{2}','{3}','{4}','{5}','{6}',{7});",codigo,nombres,apellidos,direccion,telefono,correo,fecha,id_tipo_sangre);
+            MySqlCommand query = new MySqlCommand(consulta, conectar.conectar);
+            query.Connection = conectar.conectar;
+            no = query.ExecuteNonQuery();
+
+            conectar.CerrarConexion();
+            return no;
+
+           
+        }
+        public int actualizar(int id,string codigo, string nombres, string apellidos, string direccion, string telefono, string correo, string fecha, int id_tipo_sangre)
+        {
+            int no = 0;
+            conectar = new ConexionBD();
+            conectar.AbrirConexion();
+            string consulta = string.Format("update  estudiantes set codigo='{0}',nombres='{1}',apellidos='{2}',direccion='{3}',telefono='{4}',correo_electronico='{5}',fecha_nacimiento='{6}',id_tipo_sangre={7} where id_tipo_sangre={7};", codigo, nombres, apellidos, direccion, telefono, correo, fecha, id_tipo_sangre,id);
+            MySqlCommand query = new MySqlCommand(consulta, conectar.conectar);
+            query.Connection = conectar.conectar;
+            no = query.ExecuteNonQuery();
+
+            conectar.CerrarConexion();
+            return no;
+
+
+        }
+        public int borrar(int id)
+        {
+            int no = 0;
+            conectar = new ConexionBD();
+            conectar.AbrirConexion();
+            string consulta = string.Format("delete from  estudiantes where id_tipo_sangre={0};", id);
+            MySqlCommand query = new MySqlCommand(consulta, conectar.conectar);
+            query.Connection = conectar.conectar;
+            no = query.ExecuteNonQuery();
+
+            conectar.CerrarConexion();
+            return no;
+
+
+        }
+    }
+}
---
+++ pr_web/pr_web/Estudiantes.aspx.cs
@@ -0,0 +1,83 @@
+﻿using Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace pr_web
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        Estudiante estudiante;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                estudiante = new Estudiante();
+                estudiante.drop_sangre(drop_sangre);
+                estudiante.grid_estudiates(grid_estudiantes);
+            }
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btn_crear_Click(object sender, EventArgs e)
+        {
+            estudiante=new Estudiante();
+            if (estudiante.crear(txt_codigo.Text,txt_nombres.Text,txt_apellidos.Text,txt_direccion.Text,txt_telefono.Text,txt_correo.Text,txt_fn.Text,Convert.ToInt32( drop_sangre.SelectedValue)) > 0)
+            {
+                lbl_mensaje.Text = "Ingreso Exitoso....";
+                estudiante.grid_estudiantes(grid_estudiantes);
+
+            }
+        }
+
+        protected void txt_fn_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void grid_estudiantes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txt_codigo.Text = grid_estudiantes.SelectedRow.Cells[2].Text;
+            txt_nombres.Text = grid_estudiantes.SelectedRow.Cells[3].Text;
+            txt_apellidos.Text = grid_estudiantes.SelectedRow.Cells[4].Text;
+            txt_direccion.Text =grid_estudiantes.SelectedRow.Cells[5].Text;
+            txt_telefono.Text = grid_estudiantes.SelectedRow.Cells[6].Text;
+	      txt_correo.Text = grid_estudiantes.SelectedRow.Cells[7].Text;
+            DateTime fecha = Convert.ToDateTime(grid_estudiantes.SelectedRow.Cells[8].Text);
+            txt_fn.Text= fecha.ToString("yyyy-MM-dd");
+            int indice = grid_estudiantes.SelectedRow.RowIndex;
+            drop_sangre.SelectedValue = grid_estudiantes.DataKeys[indice].Values["id_sangre"].ToString();
+            
+            btn_actualizar.Visible=true;
+        }
+
+        protected void grid_estudiantes_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+            estudiante = new Estudiante();
+            if (estudiante.borrar(Convert.ToInt32(e.Keys["id"])) > 0)
+            {
+                lbl_mensaje.Text = "Borrado Exitoso....";
+                estudiante.grid_estudiantes(grid_estudiantes);
+
+            }
+        }
+
+        protected void btn_actualizar_Click(object sender, EventArgs e)
+        {
+            estudiante = new Estudiante();
+            if (estudiante.actualizar(Convert.ToInt32(grid_estudiantes.SelectedValue), txt_codigo.Text, txt_nombres.Text, txt_apellidos.Text, txt_direccion.Text, txt_telefono.Text, txt_correo.Text, txt_fn.Text, Convert.ToInt32(drop_sangre.SelectedValue)) > 0)
+            {
+                lbl_mensaje.Text = "Modificacion Exitoso....";
+                estudiante.grid_estudiantes(grid_estudiantes);
+
+            }
+        }
+    }
+}
